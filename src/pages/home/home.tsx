e0c23d57acfce76053768b918941954975e617e9
Lloyd Carie M. Obligado <lloydobligado@gmail.com>
2023-09-26 20:43:51 +0800

    chore: add tmdb api

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,22 @@
+import { Box } from "@mui/material";
+import { LoaderComponent } from "../../components/common/loader";
+import { useTmdbApi } from "../../services/tmdb"
+
 const HomePage: React.FC = () => {
+  const { useGetMovie } = useTmdbApi()
+
+  const { data: details, isLoading } = useGetMovie();
+
+  console.log(details?.results);
+
+  if (isLoading) {
+    return (
+      <Box className="h-[calc(100vh_-_112px)]">
+        <LoaderComponent />
+      </Box>
+    );
+  }
+
   return (
     <div>HOME PAGE</div>
   )
---
+++ src/services/tmdb/types.ts
@@ -0,0 +1,27 @@
+export type Movies = {
+    page:          number;
+    results:       Result[];
+    total_pages:   number;
+    total_results: number;
+}
+
+export type Result = {
+    adult:             boolean;
+    backdrop_path:     string;
+    genre_ids:         number[];
+    id:                number;
+    original_language: OriginalLanguage;
+    original_title:    string;
+    overview:          string;
+    popularity:        number;
+    poster_path:       string;
+    release_date:      string;
+    title:             string;
+    video:             boolean;
+    vote_average:      number;
+    vote_count:        number;
+}
+
+enum OriginalLanguage {
+    En = "en",
+}
